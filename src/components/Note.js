1caf27ad078061f2ba7f864e33f6e14625eb4920
annet4588 <67749793+annet4588@users.noreply.github.com>
2022-08-08 15:38:48 +0100

    Initial commit

---
+++ src/components/Note.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(props) {
+  return (
+    <div className="note">
+      <h1 className="note-h1">{props.title}</h1>
+      <p className="note-p">{props.content}</p>
+    </div>
+  );
+}
+
+export default Note;
